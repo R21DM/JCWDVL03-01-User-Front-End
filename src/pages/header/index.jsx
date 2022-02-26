40036b5e10557bf732d4a31f5c19a9692c1efef2
alfathsien123 <alfaths77@gmail.com>
2022-02-26 20:17:28 +0700

    Login Product Landing V.1.0

--- src/pages/header/index.jsx
+++ src/pages/header/index.jsx
@@ -1,138 +1,421 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate, Navigate, Link } from "react-router-dom";
-import { Form, Button, Alert, Spinner } from "react-bootstrap";
-import "./style.css";
+import { useNavigate, Link } from "react-router-dom";
+import Axios from "axios";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Button,
+  Modal,
+  Form,
+  Alert,
+  Spinner,
+  FormControl,
+} from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../../actions/product-actions";
+import "../header/style.css";
 
 const API_URL = process.env.REACT_APP_API_URL;
-// const API_URL = "http://localhost:2000";
 
-function Navbar(props) {
+function NavigationBar() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+
   // state
   const [username, setUserName] = useState("");
   const [password, setPassWord] = useState("");
+  const [search, setSearch] = useState("");
+  const [email, setEmail] = useState("");
   const [error, setError] = useState(false);
+  const [error2, setError2] = useState(false);
+  const [confirm, setConfirm] = useState(false);
   const [loading, setLoading] = useState(false);
   const [isSignIn, setIsSignIn] = useState(false);
+  const [show, setShow] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [modalforgot, setModalForgot] = useState(false);
+  const [keep, setKeep] = useState(false);
 
-  // event handler
-  const onButtonLogin = () => {};
+  useEffect(() => {
+    const script = document.createElement("script");
+    // check token
+    const TOKEN = localStorage.getItem("token");
+    if (TOKEN) {
+      setIsSignIn(true);
+    }
 
-  // side effect -> similiar with class component's lifecycle method
-  useEffect(() => {}, []);
+    script.src = "assets/js/main.js";
+    script.async = true;
 
-  // conditional rendering : auth protection
-  if (isSignIn) {
-    return <Navigate to="/" />;
-  }
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  const close = () => {
+    setModalForgot(false);
+    setShow(true);
+    setError2(false);
+    setConfirm(false);
+  };
+  const showLogin = () => {
+    setShow(true);
+  };
+  const closeLogin = () => {
+    setShow(false);
+  };
+
+  const onButtonLogin = () => {
+    setLoading(true);
+    Axios.get(API_URL + `/users/get?username=${username}&password=${password}`)
+      .then((respond) => {
+        console.log(respond.data);
+
+        // if error -> show error
+        if (!respond.data.length) {
+          setError(true);
+          setLoading(false);
+
+          return;
+        }
+
+        // save token or id in web storage
+        localStorage.setItem("token", respond.data[0].id);
+        // data user dijadikan JWTToken, bisa diubah jadi userdata
+
+        // save data to global storage
+        dispatch({
+          type: "LOGIN",
+          payload: respond.data[0],
+        });
+
+        setLoading(false);
+        setIsSignIn(true);
+        setShow(false);
+
+        if (!keep) {
+          window.onbeforeunload = localStorage.removeItem("token");
+        }
+
+        // if success
+        navigate("/");
+
+        console.log(user);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const onButtonLogout = () => {
+    setModal(true);
+  };
+  const onButtonUserConfirm = () => {
+    // clear local storage
+    localStorage.removeItem("token");
+
+    // clear global storage
+    dispatch({ type: "LOGOUT" });
+    setModal(false);
+    window.location.reload();
+  };
+  const forget = () => {
+    setModalForgot(true);
+    setShow(false);
+  };
+  const send = () => {
+    setLoading(true);
+    setError2(false);
+    Axios.get(API_URL + `/users/get?email=${email}`)
+      .then((respond) => {
+        console.log(respond.data);
+
+        // if error -> show error
+        if (!respond.data.length) {
+          setError2(true);
+          setLoading(false);
+
+          return;
+        }
+
+        Axios.get(API_URL + `/users/send?email=${email}`)
+          .then((respond) => {
+            console.log(respond.data);
+          })
+          .catch((error) => console.log(error));
+
+        setLoading(false);
+
+        setConfirm(true);
+
+        console.log(user);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const onButtonSearch = () => {
+    navigate("/product");
+    dispatch(getProducts(search));
+    setLoading(false);
+  };
 
   return (
     <div>
-      <div id="topbar" class="d-flex align-items-center fixed-top">
-        <div class="container d-flex align-items-center justify-content-center justify-content-md-between">
-          <div class="align-items-center d-none d-md-flex">
-            <i class="bi bi-clock"></i> Monday - Saturday, 8AM to 10PM
+      <div id="topbar" className="d-flex align-items-center fixed-top">
+        <div className="container d-flex align-items-center justify-content-center justify-content-md-between">
+          <div className="align-items-center d-none d-md-flex">
+            <i className="bi bi-clock"></i> Monday - Saturday, 8AM to 10PM
           </div>
-          <div class="d-flex align-items-center">
-            <i class="bi bi-phone"></i> Call us now +62 8123 4567 891
+          <div className="d-flex align-items-center">
+            <i className="bi bi-phone"></i> Call us now +62 8123 4567 891
           </div>
         </div>
       </div>
 
       {/* --------------------------- Header ------------------------------*/}
 
-      <header id="header" class="fixed-top">
-        <div class="container d-flex align-items-center">
-          <a href="index.html" class="logo me-auto">
+      <header id="header" className="fixed-top">
+        <div className="container d-flex align-items-center">
+          <a className="logo me-auto">
             <img src="assets/img/logo.png" alt="" />
           </a>
 
-          <h1 class="logo me-auto"></h1>
+          <Navbar expand="lg" className="nav-container">
+            <Container fluid>
+              <Navbar.Brand className="nav-brand"></Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                <Nav
+                  className="me-auto my-2 my-lg-0"
+                  style={{ maxHeight: "100px" }}
+                  navbarScroll
+                ></Nav>
+                <Nav.Item style={{ marginRight: 10 }}></Nav.Item>
+                <div className="nav">
+                  {/* ------------------------------------- Search -------------------------------------*/}
 
-          <nav id="navbar" class="navbar order-last order-lg-0">
-            <ul>
-              <li>
-                <a class="nav-link scrollto" href="/">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a class="nav-link scrollto" href="#about">
-                  About
-                </a>
-              </li>
-              <li>
-                <a class="nav-link scrollto" href="#services">
-                  Services
-                </a>
-              </li>
-              <li>
-                <a class="nav-link scrollto" href="#departments">
-                  Departments
-                </a>
-              </li>
-              <li>
-                <a class="nav-link scrollto" href="#doctors">
-                  Doctors
-                </a>
-              </li>
-              <li class="dropdown">
-                <a href="#">
-                  <span>Drop Down</span> <i class="bi bi-chevron-down"></i>
-                </a>
-                <ul>
-                  <li>
-                    <a href="#">Drop Down 1</a>
-                  </li>
-                  <li class="dropdown">
-                    <a href="#">
-                      <span>Deep Drop Down</span>
-                      <i class="bi bi-chevron-right"></i>
-                    </a>
-                    <ul>
-                      <li>
-                        <a href="#">Deep Drop Down 1</a>
-                      </li>
-                      <li>
-                        <a href="#">Deep Drop Down 2</a>
-                      </li>
-                      <li>
-                        <a href="#">Deep Drop Down 3</a>
-                      </li>
-                      <li>
-                        <a href="#">Deep Drop Down 4</a>
-                      </li>
-                      <li>
-                        <a href="#">Deep Drop Down 5</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <a href="#">Drop Down 2</a>
-                  </li>
-                  <li>
-                    <a href="#">Drop Down 3</a>
-                  </li>
-                  <li>
-                    <a href="#">Drop Down 4</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a class="nav-link scrollto" href="#contact">
-                  Contact
-                </a>
-              </li>
-            </ul>
-            <i class="bi bi-list mobile-nav-toggle"></i>
-          </nav>
+                  <Form className="d-flex search">
+                    <FormControl
+                      placeholder="Search"
+                      className="me-2"
+                      aria-label="Search"
+                      value={search}
+                      onChange={(e) => setSearch(e.target.value)}
+                    />
+                    <Button variant="outline-success" onClick={onButtonSearch}>
+                      Search
+                    </Button>
+                  </Form>
 
-          <a href="#appointment" class="appointment-btn scrollto">
-            <span class="d-none d-md-inline">Make an</span> Appointment
-          </a>
+                  {/* ------------------------------------- Navigate -------------------------------------*/}
+
+                  <Nav.Link onClick={() => navigate("")}>Home</Nav.Link>
+
+                  <Nav.Link onClick={() => navigate("product")}>
+                    Products
+                  </Nav.Link>
+                  <a className="nav-link scrollto" href="#services">
+                    Services
+                  </a>
+
+                  <a className="nav-link scrollto" href="#contact">
+                    Contact
+                  </a>
+
+                  {!isSignIn ? (
+                    <Button
+                      variant="outline-info"
+                      className="button"
+                      onClick={showLogin}
+                    >
+                      Login
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="outline-success"
+                      onClick={onButtonLogout}
+                      className="button"
+                    >
+                      Logout
+                    </Button>
+                  )}
+                </div>
+              </Navbar.Collapse>
+            </Container>
+
+            {/* ------------------------------------- Login -------------------------------------*/}
+
+            <Modal show={show} onHide={closeLogin} className="modal-size">
+              <Modal.Header className="modal-header">
+                <Modal.Title className="center title">Login</Modal.Title>
+              </Modal.Header>
+
+              <Modal.Body className="">
+                <div className="form">
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter username"
+                      value={username}
+                      onChange={(e) => setUserName(e.target.value)}
+                      className="form-size"
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassWord(e.target.value)}
+                      className="form-size"
+                    />
+                  </Form.Group>
+                  {error ? (
+                    <Alert variant="danger">
+                      Error : username or password doesn't match
+                    </Alert>
+                  ) : null}
+                  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check
+                      type="checkbox"
+                      label="Remember Me"
+                      className="custom-checkbox"
+                      checked={keep}
+                      onChange={(event) => setKeep(event.target.checked)}
+                    />
+                  </Form.Group>
+                </div>
+                <div>
+                  <Button
+                    variant="primary"
+                    onClick={onButtonLogin}
+                    disabled={loading}
+                    className="button-login"
+                  >
+                    {loading ? (
+                      <Spinner animation="border" variant="light" />
+                    ) : (
+                      "Login"
+                    )}
+                  </Button>
+                </div>
+              </Modal.Body>
+
+              <Modal.Footer>
+                <h4 className="register-now register">
+                  Don't have an account?
+                  <Link to="/register" onClick={closeLogin}>
+                    {" "}
+                    Register Now
+                  </Link>
+                </h4>
+                <h4 className="register-now forgetpw">
+                  <Link to="#forgotPassword" onClick={forget}>
+                    {" "}
+                    Forget Password?
+                  </Link>
+                </h4>
+              </Modal.Footer>
+            </Modal>
+
+            {/* ------------------------------------- Forgot -------------------------------------*/}
+
+            <Modal
+              show={modalforgot}
+              onHide={closeLogin}
+              className="modal-size"
+            >
+              <Modal.Header className="modal-header">
+                <Modal.Title className="center-forgot title">
+                  Forgot Password
+                </Modal.Title>
+              </Modal.Header>
+
+              <Modal.Body className="">
+                <div className="form">
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <input
+                      type="email"
+                      placeholder="Enter Email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="form-size form-control"
+                    />
+                  </Form.Group>
+                  {error2 ? (
+                    <Alert variant="danger" className="alert">
+                      Error : can't find email
+                    </Alert>
+                  ) : confirm ? (
+                    <Alert variant="success" className="alert">
+                      Check your email!
+                    </Alert>
+                  ) : null}
+                </div>
+                <div>
+                  <Button
+                    variant="primary"
+                    onClick={send}
+                    disabled={loading}
+                    className="button-login"
+                  >
+                    {loading ? (
+                      <Spinner animation="border" variant="light" />
+                    ) : (
+                      "Send"
+                    )}
+                  </Button>
+                </div>
+                <div>
+                  <Button
+                    variant="secondary"
+                    onClick={close}
+                    className="button-cancel"
+                  >
+                    Cancel
+                  </Button>
+                </div>
+              </Modal.Body>
+            </Modal>
+
+            {/* ---------------------------------- Konfirmasi Exit -------------------------------------*/}
+
+            <Modal
+              show={modal}
+              onHide={() => setModal(false)}
+              className="modal-size2"
+            >
+              <Modal.Header closeButton>
+                <Modal.Title className="title">Konfirmasi</Modal.Title>
+              </Modal.Header>
+
+              <Modal.Body>
+                <p>Apakah anda ingin keluar ?</p>
+              </Modal.Body>
+
+              <Modal.Footer>
+                <Button variant="secondary" onClick={() => setModal(false)}>
+                  Tidak
+                </Button>
+                <Button
+                  variant="primary"
+                  onClick={onButtonUserConfirm}
+                  className="btn2"
+                >
+                  Iya
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </Navbar>
         </div>
       </header>
     </div>
   );
 }
 
-export default Navbar;
+export default NavigationBar;
---
+++ src/pages/product/index.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { getProducts } from "../../actions/product-actions";
+import { Button, Card } from "react-bootstrap";
+import "../product/style.css";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+function Product(props) {
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => {
+    return {
+      products: state.products.data,
+    };
+  });
+  const navigation = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    Axios.get(API_URL + `/products/get`)
+      .then((respond) => {
+        // console.log(respond.data);
+        dispatch(getProducts(""));
+        setLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const renderListProducts = () => {
+    console.log("Productnya", products);
+    return products.map((product) => {
+      return (
+        <Card key={product.id} id="product-card">
+          {/* <Card.Img variant="top" src={product.images[0]} /> */}
+          <Card.Body className="card-body">
+            <Card.Title>{product.name}</Card.Title>
+            <Card.Text>
+              {"Rp. "}
+              {product.price}
+              {`/${product.unit}`}
+            </Card.Text>
+            <Button
+              variant="primary"
+              onClick={() => navigation(`/products/detail/${product.id}`)}
+            >
+              Check Details
+            </Button>
+          </Card.Body>
+        </Card>
+      );
+    });
+  };
+
+  return (
+    <div>
+      {/* --------------------------- Main ------------------------------*/}
+
+      <main id="main">
+        {/* --------------------------- Product  ------------------------------*/}
+
+        <div className="product-container">
+          <div className="body-feature-product">{renderListProducts()}</div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Product;
