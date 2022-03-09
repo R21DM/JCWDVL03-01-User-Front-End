4f118f0b1d1499afbb923d87c730eed67022fda9
r21dm <rockydwimakmur@gmail.com>
2022-03-09 20:23:45 +0700

    Fix user reducer

--- src/reducers/user-reducer.js
+++ src/reducers/user-reducer.js
@@ -1,6 +1,6 @@
 import { LOGIN, LOGOUT, SIGN_IN } from "../actions/types";
 
-const INITIAL_STATE = {
+let INITIAL_STATE = {
   id: null,
   role: null,
   username: "",
@@ -14,16 +14,24 @@ const userReducer = (state = INITIAL_STATE, action) => {
       if (!action.payload) {
         return INITIAL_STATE;
       }
-      return {
+      return (INITIAL_STATE = {
         ...state,
         id: action.payload.id,
         role: action.payload.role,
         username: action.payload.username,
         email: action.payload.email,
         phone: action.payload.phone,
-      };
+      });
+
     case LOGOUT:
-      return INITIAL_STATE;
+      return (INITIAL_STATE = {
+        id: null,
+        role: null,
+        username: "",
+        email: "",
+        phone: null,
+      });
+
     case SIGN_IN:
       return {
         ...state,
